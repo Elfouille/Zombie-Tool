180405281fc3b0662329268df25cb77b13c9692f
Elfouille <145689152+Elfouille@users.noreply.github.com>
2025-12-14 18:41:48 +0100

    Add files via upload

---
+++ BO7.app/maps.js
@@ -0,0 +1,19 @@
+window.MAPS_CONFIG = {
+  title: "Zombies Tools",
+  maps: [
+    {
+      id: "map1",
+      name: "Ash of the Dammed",
+      modules: [
+        { id: "decoder", name: "Décodeur symboles", page: "modules/bo7_decoder_auto.html" }
+      ]
+    },
+    {
+      id: "map2",
+      name: "Astral",
+      modules: [
+        { id: "placeholder", name: "Module à venir", page: "modules/" }
+      ]
+    }
+  ]
+};
