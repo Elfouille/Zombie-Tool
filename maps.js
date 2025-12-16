4dbd53db9d31995252cb52bbfb49a48210ddc725
Elfouille <145689152+Elfouille@users.noreply.github.com>
2025-12-16 04:52:20 +0100

    Refactor MAPS_CONFIG with new map entries

--- maps.js
+++ maps.js
@@ -6,16 +6,24 @@ window.MAPS_CONFIG = {
       name: "Ash of the Dammed",
       modules: [
         { id: "decoder", name: "Décodeur symboles", page: "modules/bo7_decoder_auto.html" },
-	{ id: "aotd", name: "Ash of the Dammed - Carte", page: "modules/Ashes of the Damned.html" },
-	{ id: "recette", name: "Décodeur recette", page: "modules/bo7_recipe_order.html" }
+	    { id: "aotd", name: "Ash of the Dammed - Carte", page: "modules/Ashes of the Damned.html" },
+	    { id: "recette", name: "Décodeur recette", page: "modules/bo7_recipe_order.html" }
       ]
     },
 	  
     {
       id: "map2",
-      name: "Astral Malorum - Carte",
+      name: "Astral Malorum",
       modules: [
-        { id: "astral", name: "Module à venir", page: "modules/Astra Malorum.html" }
+        { id: "astral", name: "Astral Malorum - Carte", page: "modules/Astra Malorum.html" }
+      ]
+    },
+
+{
+      id: "map3",
+      name: "???",
+      modules: [
+        { id: "", name: "", page: "" }
       ]
     },
 
@@ -23,10 +31,14 @@ window.MAPS_CONFIG = {
       id: "module",
       name: "Module",
       modules: [
-        { id: "placeholder", name: "Décodeur symboles", page: "modules/" }
-      ]
+        { id: "decoder", name: "Décodeur symboles", page: "modules/bo7_decoder_auto.html" },
+	    { id: "aotd", name: "Ash of the Dammed - Carte", page: "modules/Ashes of the Damned.html" },
+	    { id: "recette", name: "Décodeur recette", page: "modules/bo7_recipe_order.html" },
+        { id: "astral", name: "Astral Malorum - Carte", page: "modules/Astra Malorum.html" }
+	  ]
     }	  
   ]
 };
+
 
 
